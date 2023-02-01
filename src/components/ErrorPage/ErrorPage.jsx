a4a1f6a9baf6e51c81f7c16ec9906f01f3d70237
nchertolin <nchertolin@gmail.com>
2023-02-02 02:37:44 +0500

    redesign 404 page

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import styles from './ErrorPage.module.scss';
 
 export default function ErrorPage() {
   return (
-    <div className='content'>
-      <h1>Ой, кажется страница не найдена</h1>
-      <Link className='primary-button' to='/'>На главную</Link>
+    <div className={styles.errorPage}>
+      <div className={styles.wrapper}>
+        <span className={styles.code}>404</span>
+        <div>
+          <h1>Cтраница не найдена</h1>
+          <p>Сайт, настроенный по этому адресу, не содержит запрашиваемую страницу.</p>
+        </div>
+        <Link className={`${styles.button} primary-button`} to='/'>На главную</Link>
+      </div>
     </div>
   )
 }
